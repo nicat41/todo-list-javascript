c02f06500128986894bdc987e144bdc0b9515b96
nicat41 <nicadhesimov871@gmail.com>
2024-03-14 01:12:52 +0400

    editItem funksiyasi hazirdi

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ const items = [
 
 ]
 
+let editIndex = "";
 
-
+let taskInput = document.querySelector("#taskInput")
 let btnSave = document.querySelector("#btnSave")
 
 
@@ -24,7 +25,7 @@ let todoContent = document.querySelector("#todoContent")
             <span class=" ${items[index].status === 'complated' ? 'line-through' : ''} ">${items[index].text}</span>
         </div>
         <div class="flex items-center space-x-[8px] ">
-            <button class=" size-[20px] bg-[#0085FF] rounded-[5px] initial-flex text-white text-[10px] ">
+            <button onclick="editItem(${index})" class=" size-[20px] bg-[#0085FF] rounded-[5px] initial-flex text-white text-[10px] ">
                 <i class="fa-solid fa-pencil "></i>
             </button>
             <button onclick="deleteItem(${index})" class=" size-[20px] bg-[#FF0000] rounded-[5px] initial-flex text-white text-[10px] ">
@@ -40,14 +41,23 @@ let todoContent = document.querySelector("#todoContent")
 createUIElement();
 
 
-saveElement = (e) => {
-let taskInput = document.querySelector("#taskinput")
+saveElement = () => {
 let value = taskInput.value;
-taskInput.value = ""
-items.push({
-    text: value,
-    status: "pending",
-})
+taskInput.value = "";
+console.log(editIndex)
+
+if (editIndex.toString()) {
+items[editIndex].text = value;
+editIndex = "";
+
+}
+else {
+    items.push({
+        text: value,
+        status: "pending",
+    })
+}
+
 createUIElement();
 
 }
@@ -63,7 +73,17 @@ const changeStatus = (index, event) => {
 }
 
 const deleteItem = (index) => {
-items.splice(index, 1)
+    const value = confirm("bunu silmek isdediyinize eminsiniz mi ?")
+    if(value) {
+        items.splice(index, 1)
+    }
 createUIElement()
 }
+
+const editItem = (index) => {
+editIndex = index;
+taskInput.value = items[index].text;
+
+}
+
 btnSave.addEventListener("click", saveElement)
